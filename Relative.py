914229c34ecee491d2af8fa26cb1ba513ba4a399
Anna Golub <cinderella_ag@mail.ru>
2020-11-30 13:19:49 +0300

    удалены файлы из основной директории

--- Relative.py
+++
@@ -1,8 +0,0 @@
-class Relative:
-    def __init__(self, id, name, color):
-        self.id = id
-        self.name = name  # русскоязычное название термина родства
-        self.color = color  # {0; 1; -1} - каким цветом будет нарисован
-
-        # заполнение шести возможных прямых связей: список id тех, с кем эта связь
-        self.relations = {'mother': [], 'father': [], 'daughter': [], 'son': [], 'husband': [], 'wife': []}
